69998bfe359437c2fa97537bfc92e1ada12f644d
Roshan Lal Sahu <sahuroshan96@gmail.com>
2025-05-23 08:48:26 +0530

    section-3 completed

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # spring-security
 
-Added SecurityFilterChain with formLogin and httpBasic and storing username and password in application.properties file
+branch -> description  
+main -> Getting started with spring-security (Hard coded values in application.properties file)  
+section-2 -> Added SecurityFilterChain with formLogin and httpBasic and storing username and password in application.properties file  
+section-3 -> Added UserDetailsService with InMemoryUserDetailsManager and PasswordEncoder
--- security/src/main/java/com/springsecurity/security/config/ProjectSecurityConfig.java
+++ security/src/main/java/com/springsecurity/security/config/ProjectSecurityConfig.java
@@ -2,8 +2,17 @@ package com.springsecurity.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -17,8 +26,28 @@ public class ProjectSecurityConfig {
                         .requestMatchers("/notices", "/contact", "/error").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
-        // http.formLogin(fl -> fl.disable());
-        // http.httpBasic(basic -> basic.disable());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withUsername("user").password("{noop}12345").authorities("read").build();
+//        admin -> 12345
+        UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$mNAL0a3lhHt7EGtTPfy6XONJ9lfgK3HrUUngQy6wrH5ecOFVANGwy").authorities("admin").build();
+//        UserDetails admin = User.withUsername("admin").password("{bcrypt}12345").authorities("admin").build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder();
+//        default is using BCryptPasswordEncoder
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+//    check if the password is compromised or not
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker(){
+        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 }
