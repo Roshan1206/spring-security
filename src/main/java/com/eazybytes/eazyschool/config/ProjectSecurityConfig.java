358d2ed32136a7b0107fda78acde80823c48d337
Roshan Lal Sahu <sahuroshan96@gmail.com>
2025-06-04 10:49:54 +0530

    form login

--- src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.eazybytes.eazyschool.config;
 
+import com.eazybytes.eazyschool.handler.CustomAuthenticationFailureHandler;
+import com.eazybytes.eazyschool.handler.CustomAuthenticationSuccessHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
@@ -15,7 +18,11 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 @Configuration
+@RequiredArgsConstructor
 public class ProjectSecurityConfig {
+
+    private final CustomAuthenticationSuccessHandler authenticationSuccessHandler;
+    private final CustomAuthenticationFailureHandler authenticationFailureHandler;
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -25,7 +32,14 @@ public class ProjectSecurityConfig {
                         .requestMatchers("/dashboard").authenticated()
                         .requestMatchers("/", "/home", "/holidays/**", "/contact", "/saveMsg",
                                 "/courses", "/about", "/assets/**", "/login/**").permitAll())
-                .formLogin(flc -> flc.loginPage("/login"))
+                .formLogin(flc -> flc
+                        .loginPage("/login")
+                        .usernameParameter("userid")
+                        .passwordParameter("secretPwd")
+                        .defaultSuccessUrl("/dashboard")
+                        .failureUrl("/login?error=true")
+                        .successHandler(authenticationSuccessHandler)
+                        .failureHandler(authenticationFailureHandler))
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
@@ -36,7 +50,7 @@ public class ProjectSecurityConfig {
         UserDetails user = User.withUsername("user")
                 .password("{noop}EazyBytes@12345").authorities("read").build();
         UserDetails admin = User.withUsername("admin")
-                .password("{bcrypt}$2a$12$88.f6upbBvy0okEa7OfHFuorV29qeK.sVbB9VQ6J6dWM1bW6Qef8m")
+                .password("{bcrypt}$2a$12$49A8hrkbCx1B2jfOpmq1zOXIM8onWGJTUU7FU5xCpOH.5clKo08EW")
                 .authorities("admin").build();
         return new InMemoryUserDetailsManager(user, admin);
     }
