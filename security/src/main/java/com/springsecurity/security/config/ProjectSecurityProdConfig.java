6fcdcbca6b956045a64a23cc0f55c4f7436f1bb9
Roshan Lal Sahu <sahuroshan96@gmail.com>
2025-06-11 10:50:47 +0530

    cors completed

--- security/src/main/java/com/springsecurity/security/config/ProjectSecurityProdConfig.java
+++ security/src/main/java/com/springsecurity/security/config/ProjectSecurityProdConfig.java
@@ -2,6 +2,7 @@ package com.springsecurity.security.config;
 
 import com.springsecurity.security.exception.CustomAccessDeniedHandler;
 import com.springsecurity.security.exception.CustomBasicAuthenticationEntryPoint;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,6 +12,10 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -21,6 +26,18 @@ public class ProjectSecurityProdConfig {
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(corsConfig -> corsConfig.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration configuration = new CorsConfiguration();
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setMaxAge(3600L);
+                        return configuration;
+                    }
+                }))
                 .sessionManagement(smc -> smc
                         .invalidSessionUrl("/invalidSession")
                         .maximumSessions(1)
