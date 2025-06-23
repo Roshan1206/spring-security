d365842905ddf0549422bf9c11c5a6d4cfa983c9
Roshan Lal Sahu <sahuroshan96@gmail.com>
2025-06-23 10:32:41 +0530

    method-level-security completed

--- security/src/main/java/com/springsecurity/security/SecurityApplication.java
+++ security/src/main/java/com/springsecurity/security/SecurityApplication.java
@@ -2,10 +2,13 @@ package com.springsecurity.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableWebSecurity(debug = true) //keep the debug false
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class SecurityApplication {
 
 	public static void main(String[] args) {
